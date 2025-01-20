42c4f5c04e1f74bc91283563fb1141c59ee002c0
ReinhyVW <110805602+ReinhyVW@users.noreply.github.com>
2025-01-20 16:10:30 -0500

    feat(Root): Created base home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-// app/page.tsx
 import { Button } from '@heroui/react';
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className='h-screen w-full flex flex-col items-center justify-center gap-8'>
+      <Button color='primary' className='max-w-xs w-full' size='lg' as={"a"} href=''>Login</Button>
+      <Button color='secondary' className='max-w-xs w-full' size='lg' as={"a"} href=''>Sign Up</Button>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,5 +12,36 @@ export default {
     extend: {},
   },
   darkMode: "class",
-  plugins: [heroui()]
+  plugins: [heroui({
+    themes: {
+      dark: {
+        colors: {
+          primary: {
+            DEFAULT: '#6e76c3',
+            '100': '#e5e7f4',
+            '200': '#d0d5ed',
+            '300': '#b0b9e0',
+            '400': '#8a95d0',
+            '500': '#6e76c3',
+            '600': '#5b5db5',
+            '700': '#514fa2',
+            '800': '#4a4687',
+            '900': '#3e3c6c',
+          },
+          secondary: {
+            DEFAULT: '#ee222b',
+            '100': '#ffdfe0',
+            '200': '#ffc5c8',
+            '300': '#ff9da1',
+            '400': '#ff646b',
+            '500': '#ff343d',
+            '600': '#ee222b',
+            '700': '#c80d15',
+            '800': '#a50f16',
+            '900': '#881419',
+          }
+        }
+      }
+    }
+  })]
 } satisfies Config;
